b4a2835135f6ad9cb45bcdc8197aec67350664a7
AxVE@gmail.com <axve34@gmail.com>
2016-04-21 15:51:16 +0200

    Ajout d'un script pour visualiser différentes informations du pc local dans tmux

---
+++ LinuxTools/Monitoring/monitoring.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+VERSION="0.1.0"
+
+#Tmux window name
+s_name="monitoring"
+
+#Specials commands
+watcher=watch -t -c "echo 'USERS:' && users && echo 'JOURNALCTL:' && journalctl --nopager -n 10"
+
+#Launch session
+tmux new -s ${s_name} -d
+
+#Set panels
+tmux split-window -v -t ${s_name}
+tmux split-window -h -t ${s_name}:0.1
+
+tmux send-keys -t ${s_name}:0.0 'htop' C-m
+tmux send-keys -t ${s_name}:0.1 'watch -t -c "echo 'USERS:' && users && echo 'JOURNALCTL:' && journalctl --no-pager -n 10"' C-m
+tmux send-keys -t ${s_name}:0.2 'dstat' C-m
+
+
+
+#Get session
+tmux attach -t ${s_name}
